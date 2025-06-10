87d495523749ffeef06523a6e933a8d12781761e
Jian Wei <itsjianweii@gmail.com>
2025-06-10 13:05:12 +0800

    Added change to reference pathname

--- src/app/_ui/fakeApiForm.tsx
+++ src/app/_ui/fakeApiForm.tsx
@@ -6,13 +6,18 @@ import { addData } from "../_lib/data-service";
 import { generateId } from "../_utils/helper";
 import { toast } from "sonner";
 import Spinner from "../_components/Spinner";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Text from "../_components/Text";
 
 export default function FakeApiForm() {
+  const [pathname, setPathname] = useState("");
   const [url, setUrl] = useState("");
   const [data, setData] = useState("");
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setPathname(window.location.href);
+  }, []);
 
   async function handleDataSubmit() {
     setIsLoading(true);
@@ -21,7 +26,7 @@ export default function FakeApiForm() {
       const id = await addData(generateId(), jsonData);
       toast.success("Completed. Your API endpoint has been created.");
       setData("");
-      setUrl("http://localhost:3000/mockapi/api/" + id);
+      setUrl(pathname + "/api/" + id);
     } catch (err) {
       console.error(err);
       toast.error("Invalid JSON data. Please try again.");
