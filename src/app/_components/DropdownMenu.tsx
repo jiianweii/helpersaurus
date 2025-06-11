6721daf04c2c02cb0a0a39c06523f383395c624c
Jian Wei <itsjianweii@gmail.com>
2025-06-11 18:02:43 +0800

    fixed dropdown background color

--- src/app/_components/DropdownMenu.tsx
+++ src/app/_components/DropdownMenu.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function DropdownMenu({ urlParam }: { urlParam: string }) {
   return (
-    <div className="border-3 border-[#000] rounded-2xl overflow-hidden absolute bottom-[-100px] right-0">
+    <div className="border-3 border-[#000] rounded-2xl overflow-hidden absolute bottom-[-100px] right-0 bg-white">
       <div className="flex flex-col mt-[-5px] w-full">
         <Link
           href="/mockapi"
